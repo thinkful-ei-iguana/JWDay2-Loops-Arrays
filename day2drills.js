d26d4f23d699608c98aecb9b74cb4bc6d13123ea
Wendy Bartos <wendyebartos@gmail.com>
2019-10-22 09:25:08 -0700

    round2

---
+++ day2drills.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function max(num){
+
+  let newMax = num[0];
+  //console.log(num[0]);
+
+  let i = 0;
+  while(i < num.length){
+    if(newMax < num[i]){
+      newMax = num[i];
+    }
+    i++;
+  }
+  console.log(newMax);
+  return newMax;
+}
+max([1,2,3,4,5,6,7,]);
+
+function min (arr) {
+  let newMin = arr[0];
+  let i = 0;
+  while(i < arr.length) {
+    if (newMin > arr[i]) {
+      newMin = arr[i];
+    }
+    i++;
+  }
+  return newMin;
+}
+console.log(min([10,3,6,5,7]));
